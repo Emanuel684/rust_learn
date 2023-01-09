e2ea89d7448e08912a6310c7675b79dcbf44a756
Emanuel684 <emanuelacag@gmail.com>
2023-01-08 21:30:03 -0500

    feat: Initial commit

---
+++ hello_world/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // De esta forma se hacen print
+    println!("Hello, world!");
+    // let valor2 = production_rate_per_hour(4);
+    // println!("{}", valor2);
+}
+
+pub fn nombre_creador() {
+    let nombre_creador = "Emanuel Acevedo";
+    println!("{}", nombre_creador);
+}
+
+// pub fn production_rate_per_hour(speed: u8) -> f64 {
+//     // unimplemented!("calculate hourly production rate at speed: {}", speed)
+//     let valor:u8 = speed * 221;
+//     return valor as f64;
+// }
+
+// pub fn working_items_per_minute(speed: u8) -> u32 {
+//     // unimplemented!("calculate the amount of working items at speed: {}", speed)
+// }
